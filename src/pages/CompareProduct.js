50e9a618a19af4b7902028124281ae3ac7b3de8f
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-09-10 23:56:02 +0530

    create compareProduct page

---
+++ src/pages/CompareProduct.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BreadCrumb from '../components/BreadCrumb'
+import  Meta  from '../components/Meta'
+
+const CompareProduct = () => {
+  return (
+    <>
+    <Meta title={'Compare Products'}/>
+    <BreadCrumb title="Compare Products" />
+    </>
+  )
+}
+
+export default CompareProduct
