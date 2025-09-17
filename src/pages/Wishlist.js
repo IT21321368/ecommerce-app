6d5911b065960e792fbb60d19d60c9042c82e4a5
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-09-17 23:59:39 +0530

    complete wishlist page

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -16,6 +16,34 @@ const Wishlist = () => {
                         <div className='wishlist-card-image'>
                             <img src='images/watch.jpg' alt='watch' className='img-fluid w-100'/>
                         </div>
+                        <div className='py-3 px-3'>
+                            <h5 className='title'>Apple Watch Series 7</h5>
+                            <h6 className='price'>$ 399.00</h6>
+                        </div>
+                    </div>
+                </div>
+                                <div className='col-3'>
+                    <div className='wishlist-card position-relative'>
+                        <img src='images/cross.svg' alt='cross' className='position-absolute cross img-fluid'/>
+                        <div className='wishlist-card-image'>
+                            <img src='images/watch.jpg' alt='watch' className='img-fluid w-100'/>
+                        </div>
+                        <div className='py-3 px-3'>
+                            <h5 className='title'>Apple Watch Series 7</h5>
+                            <h6 className='price'>$ 399.00</h6>
+                        </div>
+                    </div>
+                </div>
+                                <div className='col-3'>
+                    <div className='wishlist-card position-relative'>
+                        <img src='images/cross.svg' alt='cross' className='position-absolute cross img-fluid'/>
+                        <div className='wishlist-card-image'>
+                            <img src='images/watch.jpg' alt='watch' className='img-fluid w-100'/>
+                        </div>
+                        <div className='py-3 px-3'>
+                            <h5 className='title'>Apple Watch Series 7</h5>
+                            <h6 className='price'>$ 399.00</h6>
+                        </div>
                     </div>
                 </div>
             </div>
