3dfc4f57f66b5811ea4db14fbaaef50769318dd2
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-09-22 23:21:58 +0530

    create checkout  page

---
+++ src/pages/Checkout.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BiArrowBack } from 'react-icons/bi'
+import watch from '../images/watch.jpg'
+
+const Checkout = () => {
+  return (
+    <>
+    <div className='checkout-wrapper py-5 home-wrapper-2'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-7'>
+            <div className='checkout-left-data'>
+                <h3 className='website-name'>Dev Corner</h3>
+                <nav style={{"--bs-breadcrumb-divider": "'>'"}} aria-label="breadcrumb">
+                  <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <Link to="/cart" className='text-dark total-price'>Cart</Link></li>
+                    <li className="breadcrumb-item active total-price" aria-current="page">Information</li>
+                    <li className="breadcrumb-item active total-price">Shipping</li>
+                    <li className="breadcrumb-item active total-price" aria-current="page">Payment</li>
+                  </ol>
+                </nav>
+                <h4 className='title total'>Contact Information</h4>
+                <p className='user-details total'>Dev Corner (devcorner@example.com)</p>
+                <h4 className='mb-3'>Shipping Address</h4>
+                <form action="" className='d-flex flex-wrap gap-15 justify-content-between'>
+                    <div className='w-100'>
+                        <select name="" className='form-control form-select' id=''>
+                            <option value="" selected disabled>-- Select Country --</option>
+                        </select>
+                    </div>
+                  <div className='flex-grow-1'>
+                    <input type="text" className='form-control' id="name" placeholder='First Name' />
+                  </div>
+                  <div className='flex-grow-1'>
+                    <input type="text" className='form-control' id="name" placeholder='Last Name' />
+                  </div>
+                  <div className='w-100'>
+                    <input type="text" className='form-control' id="name" placeholder='Address' />
+                  </div>
+                  <div className='w-100'>
+                    <input type="text" className='form-control' id="name" placeholder='Apartment, suite, etc.' />
+                  </div>
+                  <div className='flex-grow-1'>
+                    <input type="text" className='form-control' id="name" placeholder='City' />
+                  </div>
+                  <div className='flex-grow-1'>
+                    <select name="" className='form-control form-select' id=''>
+                        <option value="" selected disabled>-- Select State --</option>
+                    </select>
+                  </div>
+                  <div className='flex-grow-1'>
+                    <input type="text" className='form-control' id="name" placeholder='Zip / Postal Code' />
+                  </div>
+                  <div className='w-100'>
+                    <div className='d-flex justify-content-between align-items-center'>
+                        <Link to='/cart' className='text-dark'><BiArrowBack /> Return to Cart</Link>
+                        <Link to='/cart' className='button'>Continue to Shipping</Link>
+                    </div>
+                  </div>
+                  
+                </form>
+            </div>
+          </div>
+          <div className='col-5'>
+            <div className='border-bottom py-4'>
+                <div className='d-flex gap-10 mb-2 align-items-center'>
+                    <div className='w-75 d-flex gap-10'>
+                        <div className='w-25 position-relative'>
+                            <span style={{top: "-10px", right: "2px"}} className='badge bg-secondary text-white rounded-circle p-2 position-absolute'>1</span>
+                            <img className='img-fluid' src={watch} alt="product" />
+                        </div>
+                        <div>
+                            <h5 className='total-price'>Product Name</h5>
+                            <p className='total-price'>Size: M</p>
+                        </div>
+                    </div>
+                    <div className='flex-grow-1'>
+                        <h5 className='total'>$ 100</h5>
+                    </div>
+                </div>
+                
+            </div>
+            <div className='border-bottom py-4'>
+                <div className='d-flex justify-content-between align-items-center'>
+                    <p className='total'>Subtotal</p>
+                    <p className='total-price'>$ 1000.00</p>
+                </div>
+                <div className='d-flex justify-content-between align-items-center'>
+                    <p className='mb-0 total'>Shipping</p>
+                    <p className='mb-0 total-price'>$ 1000.00</p>
+                </div>
+            </div>
+            
+            <div className='d-flex justify-content-between align-items-center border-bottom py-4'>
+                <h4 className='total'>Total</h4>
+                <h5 className='total-price'>$ 1000.00</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Checkout
