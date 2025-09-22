40d04aeb2fef8c237e1939f476f653323f0297d5
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-09-22 19:43:08 +0530

    update images

--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -3,6 +3,7 @@ import BreadCrumb from '../components/BreadCrumb'
 import  Meta  from '../components/Meta'
 import { HiOutlineArrowLeft } from 'react-icons/hi'
 import { Link } from 'react-router-dom'
+import blog from '../images/blog-1.jpg'
 
 
 const SingleBlog = () => {
@@ -18,7 +19,7 @@ const SingleBlog = () => {
                         <Link to='/blogs' className='d-flex align-items-center gap-10'>
                         <HiOutlineArrowLeft className='fs-4' /> Go Back To Blogs</Link>
                         <h3 className='title'>A Beautiful Sunday Morning Renaissance</h3>
-                        <img src='images/blog-1.jpg' className='img-fluid w-100 my-4' alt='blog'/>
+                        <img src={blog} className='img-fluid w-100 my-4' alt='blog'/>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla</p>
                     </div>
